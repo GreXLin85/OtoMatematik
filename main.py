25a092e691d95d34e55d95cb54240670ab46e655
GreXLin85 <38141908+GreXLin85@users.noreply.github.com>
2018-05-18 21:54:59 +0300

    Update main.py
    Birkaç bişeyler ekledim xd

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from math import sqrt
 a = input("""
-[1] Çevre bulma (Çember)
-[2] Eğim bulma
+[*]Toplama # yapım aşamasında
+[*]Çıkarma # yapım aşamasında
+[*]Çarpma # yapım aşamasında
+[*]Bölme # yapım aşamasında
+[5]Çevre bulma (Çember)
+[6]Eğim bulma
+[7]Karekök 
 
 [99]Program hakkında
 [0]Programdan çık
@@ -17,15 +23,15 @@ if(a == "99"):
   http://www.matematikciler.com/8-sinif-egim-ve-dogrunun-egimi/
   http://www.matematiktutkusu.com/extra/iljijiljiljiljljiljil/cevre-formulleri.htm
   """)
-if (a == "1"):
+
+if (a == "5"):
   pi = int(input("Pi : "))
   yari_cap = int(input("Yarı Çap : "))
   cevre = 2*pi*yari_cap
   print(cevre)
   print("""Yapılan işlem
 2*pi*yarı çap""")
-
-if(a == "2"):
+if(a == "6"):
   du = int(input("Dikey uzunluk : "))
   yu = int(input("Yatay uzunluk : "))
   egim = du/yu
@@ -33,3 +39,7 @@ if(a == "2"):
   print("""Yapılan işlem
 m = dikey uzunluk/yatay uzunluk
 """)
+if (a == "7"):
+	kk = input("Karekök bulunacak sayı : ")
+	kkb = sqrt(kk)
+	print(kkb)
